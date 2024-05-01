fa2906767855c499ec8ffe413e86ece444019c82
Ry <ryanst755@gmail.com>
2024-04-30 22:07:04 -0700

    bg: Specify background image file on command line

--- applications/bg/main.asm
+++ applications/bg/main.asm
@@ -2,11 +2,12 @@
 
     opton
 
-    ; open the background file and draw it
+    ; open the specified file and draw it
     ; if the file can't be opened, then just exit
     call get_current_disk_id
     mov r1, r0
-    mov r0, bg_file_name
+    pop r0
+    pop r0
     mov r2, bg_file_struct
     call open
     cmp r0, 0
@@ -20,7 +21,6 @@
 
     call end_current_task
 
-bg_file_name: data.strz "bg.raw"
 bg_file_struct: data.fill 0, 32
 
     #include "../../../fox32rom/fox32rom.def"
