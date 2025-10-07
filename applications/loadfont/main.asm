c1574dc8e2a856d288d33c3e2cc8457769365ac0
Ry <ryanst755@gmail.com>
2025-10-06 18:24:58 -0700

    loadfont: Print errors, don't silently fail

--- applications/loadfont/main.asm
+++ applications/loadfont/main.asm
@@ -4,7 +4,7 @@
     pop [file_name_ptr]
 
     cmp [file_name_ptr], 0
-    ifz call end_current_task
+    ifz rjmp arg_error
 
     call get_current_disk_id
     mov r1, r0
@@ -12,13 +12,13 @@
     mov r2, file_struct
     call open
     cmp r0, 0
-    ifz call end_current_task
+    ifz rjmp file_error
     mov r0, file_struct
     call get_size
     push r0
     call allocate_memory
     cmp r0, 0
-    ifz call end_current_task
+    ifz rjmp memory_error
     mov [font_buffer_ptr], r0
     pop r0
     mov r1, file_struct
@@ -30,10 +30,30 @@
 
     call end_current_task
 
+arg_error:
+    mov r0, arg_error_str
+    rjmp.8 error
+file_error:
+    mov r0, file_error_str
+    rjmp.8 error
+memory_error:
+    mov r0, memory_error_str
+error:
+    mov r2, r0
+    call string_length
+    mov r1, [stream_ptr]
+    call write
+
+    call end_current_task
+
 stream_ptr: data.32 0
 file_name_ptr: data.32 0
 file_struct: data.fill 0, 32
 font_buffer_ptr: data.32 0
+
+arg_error_str: data.str "usage: loadfont <file path>" data.8 10 data.8 0
+file_error_str: data.str "could not open file" data.8 10 data.8 0
+memory_error_str: data.str "not enough available memory" data.8 10 data.8 0
 
     #include "../../../fox32rom/fox32rom.def"
     #include "../../fox32os.def"
