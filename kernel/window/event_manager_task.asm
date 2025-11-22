fox32-arch/fox32os

commit 1c3c6db8be812bf05e5999a69b5a5db79ad4b234

kernel: Slightly smarter window ordering logic
This should hopefully resolve the weird ordering behavior, as well as
support making the desktop window (WINDOW_FLAG_ALWAYS_BACKGROUND) be the
active window with its own menu bar.

kernel/window/event_manager_task.asm
@@ -137,18 +137,32 @@ event_manager_task_mouse_event_inactive_window_was_clicked:
     ; then assume the click was on an inactive window marked as "always background"
     ; it's probably bad to assume this, more checks would be good
     cmp r0, 0x00000000
-    ifz jmp event_manager_task_mouse_event_inactive_window_was_clicked_no_change
+    ifz jmp event_manager_task_mouse_event_inactive_window_was_clicked_front_without_swap
 
-    ; swap the two, if the "always background" flag is not set for the clicked window
+    ; if the "always background" flag is set for the clicked window, don't swap
     push r1
-    add r1, 26
-    movz.16 r1, [r1]
+    movz.16 r1, [r1+26]
     and r1, WINDOW_FLAG_ALWAYS_BACKGROUND
     cmp r1, 0
     pop r1
-    ifnz jmp event_manager_task_mouse_event_inactive_window_was_clicked_no_change
-    call swap_windows
+    ifnz jmp event_manager_task_mouse_event_inactive_window_was_clicked_front_without_swap
 
+    ; if the "always background" flag is set for the active window, swap the clicked
+    ; window with the window linked to the highest enabled overlay ID
+    push r0
+    movz.16 r0, [r0+26]
+    and r0, WINDOW_FLAG_ALWAYS_BACKGROUND
+    cmp r0, 0
+    pop r0
+    ifnz jmp event_manager_task_mouse_event_inactive_window_was_clicked_front_swap_highest
+    call swap_windows
+event_manager_task_mouse_event_inactive_window_was_clicked_front_swap_highest:
+    ; swap the clicked window with the window linked to the highest enabled overlay ID
+    push r0
+    call get_window_with_highest_overlay
+    call swap_windows
+    pop r0
+event_manager_task_mouse_event_inactive_window_was_clicked_front_without_swap:
     ; mark the clicked window as the active window
     push r0
     mov r0, r1
@@ -158,9 +172,11 @@ event_manager_task_mouse_event_inactive_window_was_clicked:
 
     ; redraw the title bars of both windows
     mov r0, r1
-    call draw_title_bar_to_window
+    cmp r0, 0
+    ifnz call draw_title_bar_to_window
     pop r0
-    call draw_title_bar_to_window
+    cmp r0, 0
+    ifnz call draw_title_bar_to_window
 
     ; set the menu bar for the newly active window
     mov r0, r2
@@ -177,15 +193,5 @@ event_manager_task_mouse_event_inactive_window_was_clicked:
     pop r0
     call add_mouse_event_to_active_window
     ret
-event_manager_task_mouse_event_inactive_window_was_clicked_no_change:
-    mov [old_r8], r8
-    mov r8, r1
-    pop r2
-    pop r1
-    pop r0
-    call add_mouse_event_to_inactive_window
-    mov r8, [old_r8]
-    ret
-old_r8: data.32 0
 
 event_manager_task_name: data.strz "eventmgr"

kernel/window/window.asm
@@ -872,29 +872,54 @@ search_for_nonempty_window_list_entry_found:
 get_window_with_overlay:
     push r1
     push r2
-    push r3
     push r31
 
     mov r1, window_list
     mov r31, 31
 get_window_with_overlay_loop:
     mov r2, [r1]
-    add r2, 24
-    cmp.8 [r2], r0
+    cmp.8 [r2+24], r0
     ifz jmp get_window_with_overlay_found
     add r1, 4
-    loop get_window_with_overlay_loop
+    rloop get_window_with_overlay_loop
     ; not found, return 0
     mov r0, 0
 
     pop r31
-    pop r3
     pop r2
     pop r1
     ret
 get_window_with_overlay_found:
     ; found the entry, return the pointer to its struct
     mov r0, [r1]
+
+    pop r31
+    pop r2
+    pop r1
+    ret
+
+; get the window struct of the window associated with the highest enabled overlay
+; inputs:
+; none
+; outputs:
+; r0: pointer to window struct, or 0x00000000 if not found
+get_window_with_highest_overlay:
+    push r1
+    push r2
+    push r3
+    push r31
+
+    mov r0, 0 ; highest found so far
+    mov r1, window_list
+    mov r31, 31
+get_window_with_highest_overlay_loop:
+    mov r2, [r1]
+    cmp.8 [r2+24], r0
+    ifgt movz.8 r0, [r2+24]
+    ifgt mov r3, r2
+    inc r1, 4
+    rloop get_window_with_highest_overlay_loop
+    mov r0, r3
 
     pop r31
     pop r3

commit 5fb96d12851a43d4b98d392b9b149db81817b78d

kernel: Fix major windowing errors and crashes introduced in 1c3c6db

kernel/window/event_manager_task.asm
@@ -160,7 +160,9 @@ event_manager_task_mouse_event_inactive_window_was_clicked_front_swap_highest:
     ; swap the clicked window with the window linked to the highest enabled overlay ID
     push r0
     call get_window_with_highest_overlay
-    call swap_windows
+    ; ensure we actually got a window struct
+    cmp r0, 0
+    ifnz call swap_windows
     pop r0
 event_manager_task_mouse_event_inactive_window_was_clicked_front_without_swap:
     ; mark the clicked window as the active window

kernel/window/window.asm
@@ -902,7 +902,7 @@ get_window_with_overlay_found:
 ; inputs:
 ; none
 ; outputs:
-; r0: pointer to window struct, or 0x00000000 if not found
+; r0: pointer to window struct, or 0x00000000 if error
 get_window_with_highest_overlay:
     push r1
     push r2
@@ -911,12 +911,16 @@ get_window_with_highest_overlay:
 
     mov r0, 0 ; highest found so far
     mov r1, window_list
+    mov r3, 0
     mov r31, 31
 get_window_with_highest_overlay_loop:
     mov r2, [r1]
+    cmp r2, 0
+    ifz rjmp.8 get_window_with_highest_overlay_loop_next
     cmp.8 [r2+24], r0
-    ifgt movz.8 r0, [r2+24]
-    ifgt mov r3, r2
+    ifgteq movz.8 r0, [r2+24]
+    ifgteq mov r3, r2
+get_window_with_highest_overlay_loop_next:
     inc r1, 4
     rloop get_window_with_highest_overlay_loop
     mov r0, r3